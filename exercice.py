d4f18adf951835ed9a7baee35612675f6fa88c65
RTorrella <rene.torrella@polymtl.ca>
2022-05-10 21:44:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,8 +18,6 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    dd= d + m/60 + s/3600
-r=(dd*pi)/180
     return 0.0
 
 
