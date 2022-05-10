cf209e3f9812763a1096b853286d68e97220918c
RTorrella <rene.torrella@polymtl.ca>
2022-05-10 21:43:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def square(a: float) -> float:
 
 def average(a: float, b: float, c: float) -> float:
     list([a,b,c])
-    return sum(iterable)
+    return sum(list)/3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
